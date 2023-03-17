1bbb26f21d40ab14ad26f6eebd6f4636225b1181
Eduardo Costa <m4c0@users.noreply.github.com>
2023-03-17 12:33:16 -0300

    initial module with casein

--- build.cpp
+++ build.cpp
@@ -1,8 +1,13 @@
+#include "../casein/build.hpp"
 #include "ecow.hpp"
 
 using namespace ecow;
 
 int main(int argc, char **argv) {
+  auto m = unit::create<mod>("sokoban");
+  m->add_wsdep("casein", casein());
+
   auto a = unit::create<app>("sokoban");
+  a->add_ref(m);
   return run_main(a, argc, argv);
 }
---
+++ sokoban.cppm
@@ -0,0 +1,4 @@
+export module sokoban;
+import casein;
+
+extern "C" void casein_handle(const casein::event &e) {}
