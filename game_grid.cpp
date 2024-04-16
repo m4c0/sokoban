460051c6b511718a2d7c68547ba98faeae18b26b
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-16 11:12:07 +0100

    game_grid: pull event handler

--- game_grid.cpp
+++ game_grid.cpp
@@ -2,11 +2,24 @@ module sokoban;
 import :grid;
 
 namespace sg = sokoban::game;
-namespace sgg = sokoban::game_grid;
 namespace sl = sokoban::levels;
 namespace sr = sokoban::renderer;
 
 static unsigned m_level{};
+
+void set_level(unsigned idx) {
+  m_level = idx % sl::max_levels();
+
+  auto lvl = sl::level(idx);
+  sg::grid.load(lvl);
+
+  sg::player_pos = 0;
+  while (lvl[sg::player_pos] != 'P')
+    sg::player_pos++;
+
+  sr::render();
+  streamer::instance().play(50);
+}
 
 static void move(unsigned p) {
   switch (auto mt = sg::grid.move_type(sg::player_pos, p)) {
@@ -19,7 +32,7 @@ static void move(unsigned p) {
     sg::grid.set_box(sg::player_pos + p);
     sg::grid.clear_box(sg::player_pos);
     if (sg::grid.is_done()) {
-      sgg::set_level(m_level + 1);
+      set_level(m_level + 1);
     } else if (mt == push2tgt) {
       streamer::instance().play(100);
     } else {
@@ -33,22 +46,61 @@ static void move(unsigned p) {
   sr::render();
 }
 
-void sgg::set_level(unsigned idx) {
-  m_level = idx % sl::max_levels();
+void reset_level() { set_level(m_level); }
 
-  auto lvl = sl::level(idx);
-  sg::grid.load(lvl);
+void down() { move(sl::level_width); }
+void up() { move(-sl::level_width); }
+void left() { move(-1); }
+void right() { move(1); }
 
-  sg::player_pos = 0;
-  while (lvl[sg::player_pos] != 'P')
-    sg::player_pos++;
-
-  sr::render();
-  streamer::instance().play(50);
+static class : public casein::handler {
+public:
+  void create_window(const casein::events::create_window &e) override {
+    set_level(0);
+  }
+  void gesture(const casein::events::gesture &e) override {
+    switch (*e) {
+    case casein::G_SWIPE_UP:
+      up();
+      break;
+    case casein::G_SWIPE_DOWN:
+      down();
+      break;
+    case casein::G_SWIPE_LEFT:
+      left();
+      break;
+    case casein::G_SWIPE_RIGHT:
+      right();
+      break;
+    case casein::G_SHAKE:
+      reset_level();
+      break;
+    default:
+      break;
+    }
+  }
+  void key_down(const casein::events::key_down &e) override {
+    switch (*e) {
+    case casein::K_UP:
+      up();
+      break;
+    case casein::K_DOWN:
+      down();
+      break;
+    case casein::K_LEFT:
+      left();
+      break;
+    case casein::K_RIGHT:
+      right();
+      break;
+    case casein::K_SPACE:
+      reset_level();
+      break;
+    default:
+      break;
+    }
+  }
+} evt;
+void sokoban::game_grid::process_event(const casein::event &e) {
+  evt.handle(e);
 }
-void sgg::reset_level() { set_level(m_level); }
-
-void sgg::down() { move(sl::level_width); }
-void sgg::up() { move(-sl::level_width); }
-void sgg::left() { move(-1); }
-void sgg::right() { move(1); }
--- sokoban.cppm
+++ sokoban.cppm
@@ -10,13 +10,7 @@ import casein;
 import quack;
 
 namespace sokoban::game_grid {
-void set_level(unsigned);
-void reset_level();
-
-void down();
-void up();
-void left();
-void right();
+void process_event(const casein::event &e);
 } // namespace sokoban::game_grid
 
 namespace sokoban::levels {
@@ -36,60 +30,11 @@ extern class grid grid;
 extern unsigned player_pos;
 } // namespace sokoban::game
 
-static class : public casein::handler {
-public:
-  void create_window(const casein::events::create_window &e) override {
-    sokoban::game_grid::set_level(0);
-  }
-  void gesture(const casein::events::gesture &e) override {
-    switch (*e) {
-    case casein::G_SWIPE_UP:
-      sokoban::game_grid::up();
-      break;
-    case casein::G_SWIPE_DOWN:
-      sokoban::game_grid::down();
-      break;
-    case casein::G_SWIPE_LEFT:
-      sokoban::game_grid::left();
-      break;
-    case casein::G_SWIPE_RIGHT:
-      sokoban::game_grid::right();
-      break;
-    case casein::G_SHAKE:
-      sokoban::game_grid::reset_level();
-      break;
-    default:
-      break;
-    }
-  }
-  void key_down(const casein::events::key_down &e) override {
-    switch (*e) {
-    case casein::K_UP:
-      sokoban::game_grid::up();
-      break;
-    case casein::K_DOWN:
-      sokoban::game_grid::down();
-      break;
-    case casein::K_LEFT:
-      sokoban::game_grid::left();
-      break;
-    case casein::K_RIGHT:
-      sokoban::game_grid::right();
-      break;
-    case casein::K_SPACE:
-      sokoban::game_grid::reset_level();
-      break;
-    default:
-      break;
-    }
-  }
-} evt;
-
 extern "C" void casein_handle(const casein::event &e) {
   streamer::instance();
   quack::mouse_tracker::instance().handle(e);
   sokoban::renderer::process_event(e);
-  evt.handle(e);
+  sokoban::game_grid::process_event(e);
 }
 
 module :private;
