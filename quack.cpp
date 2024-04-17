8c13e83ed9305d78f9b8ea08e1255f32d983de1a
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-17 17:09:27 +0100

    quack: reducing boilerplate

--- quack.cpp
+++ quack.cpp
@@ -9,73 +9,67 @@ import voo;
 namespace sg = sokoban::game;
 namespace sl = sokoban::levels;
 
-class updater : public quack::instance_batch_thread {
-  // {{{ quad map utils
-  static constexpr auto uv(atlas_sprites s) {
-    constexpr const auto h = 1.0f / static_cast<float>(sprite_count);
-    const auto n = static_cast<unsigned>(s);
-    return quack::uv{{0, n * h}, {1, (n + 1) * h}};
+// {{{ quad map utils
+static constexpr auto uv(atlas_sprites s) {
+  constexpr const auto h = 1.0f / static_cast<float>(sprite_count);
+  const auto n = static_cast<unsigned>(s);
+  return quack::uv{{0, n * h}, {1, (n + 1) * h}};
+}
+static constexpr auto uv(char b) {
+  switch (b) {
+  case box:
+  case target_box:
+    return uv(sprite_box);
+  case target:
+    return uv(sprite_target);
+  case player:
+  case player_target:
+    return uv(sprite_player);
+  default:
+    return uv(sprite_empty);
   }
-  static constexpr auto uv(char b) {
-    switch (b) {
-    case box:
-    case target_box:
-      return uv(sprite_box);
-    case target:
-      return uv(sprite_target);
-    case player:
-    case player_target:
-      return uv(sprite_player);
-    default:
-      return uv(sprite_empty);
-    }
+}
+static constexpr auto colour(char b) {
+  switch (b) {
+  case target_box:
+  case target:
+  case player_target:
+    return quack::colour{0, 0.7, 0, 1};
+  case wall:
+    return quack::colour{0, 0, 1, 1};
+  case outside:
+    return quack::colour{};
+  case box:
+  case player:
+  case empty:
+    return quack::colour{0, 0.3, 0, 1};
   }
-  static constexpr auto colour(char b) {
-    switch (b) {
-    case target_box:
-    case target:
-    case player_target:
-      return quack::colour{0, 0.7, 0, 1};
-    case wall:
-      return quack::colour{0, 0, 1, 1};
-    case outside:
-      return quack::colour{};
-    case box:
-    case player:
-    case empty:
-      return quack::colour{0, 0.3, 0, 1};
+  return quack::colour{1, 0, 1, 1};
+}
+// }}}
+
+void update_data(quack::mapped_buffers all) {
+  auto [c, m, p, u] = all;
+  // {{{ quad memory map
+  auto i = 0U;
+  for (char b : sg::grid) {
+    if (sg::player_pos == i) {
+      b = (b == target) ? player_target : player;
     }
-    return quack::colour{1, 0, 1, 1};
+    float x = i % sl::level_width;
+    float y = i / sl::level_width;
+    *p++ = {{x, y}, {1, 1}};
+    *u++ = uv(b);
+    *c++ = colour(b);
+    *m++ = quack::colour{1, 1, 1, 1};
+    i++;
   }
   // }}}
+}
 
-  void update_data(all all) override {
-    auto [c, m, p, u] = all;
-    // {{{ quad memory map
-    auto i = 0U;
-    for (char b : sg::grid) {
-      if (sg::player_pos == i) {
-        b = (b == target) ? player_target : player;
-      }
-      float x = i % sl::level_width;
-      float y = i / sl::level_width;
-      *p++ = {{x, y}, {1, 1}};
-      *u++ = uv(b);
-      *c++ = colour(b);
-      *m++ = quack::colour{1, 1, 1, 1};
-      i++;
-    }
-    // }}}
-  }
-
-public:
-  // {{{ max quads
-  const unsigned max_quads() { return sl::level_width * sl::level_height; }
-  // }}}
-
-  explicit updater(voo::device_and_queue *dq, quack::pipeline_stuff &ps)
-      : instance_batch_thread{dq->queue(), ps.create_batch(max_quads())} {}
-};
+// {{{ max quads
+const unsigned max_quads() { return sl::level_width * sl::level_height; }
+// }}}
 
 static volatile bool dirty{true};
 static struct : public voo::casein_thread {
@@ -86,7 +80,8 @@ static struct : public voo::casein_thread {
 
     voo::device_and_queue dq{app_name, native_ptr()};
     quack::pipeline_stuff ps{dq, 1};
-    updater u{&dq, ps};
+    quack::instance_batch_thread u{dq.queue(), ps.create_batch(max_quads()),
+                                   &update_data};
 
     atlas_img a{&dq};
     a.run_once();
