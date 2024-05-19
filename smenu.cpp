ab63ebe142b0bf3d6f192a245d3dc4ab07df675d
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-19 12:21:15 +0100

    smenu: composite ui render

--- smenu.cpp
+++ smenu.cpp
@@ -1,24 +1,24 @@
 module sokoban;
+import sprites;
+
+static unsigned dim(quack::mapped_buffers &all) {
+  auto &[c, m, p, u] = all;
+  float w = sl::level_width * 2.0;
+  float h = sl::level_height * 2.0;
+
+  *p++ = {{-w, -h}, {w * 2.0f, h * 2.0f}};
+  *u++ = {};
+  *c++ = {0, 0, 0, 0.9};
+  *m++ = {1, 1, 1, 1};
+  return 1;
+}
 
 static unsigned update_data(quack::mapped_buffers all) {
   auto count = sr::update_data(all);
 
-  auto [c, m, p, u] = all;
-
-  float w = sl::level_width * 2.0;
-  float h = sl::level_height * 2.0;
-
-  p[count] = {{-w, -h}, {w * 2.0f, h * 2.0f}};
-  u[count] = {};
-  c[count] = {0, 0, 0, 0.9};
-  m[count] = {1, 1, 1, 1};
-  count++;
-
-  p[count] = {{0, 0}, {2, 1}};
-  u[count] = {{0.25, 0.0}, {0.75, 0.25}};
-  c[count] = {0, 0, 0, 0};
-  m[count] = {1, 1, 1, 1};
-  count++;
+  count += dim(all);
+  count += spr::blit::level(all, 2, 2);
+  count += spr::blit::number(all, sl::current_level(), 5, 2);
 
   return count;
 }
