788889342cf5ab1f15f00ec826b4891f54caffbd
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-21 12:25:31 +0100

    sselect: draw level numbers

--- sselect.cpp
+++ sselect.cpp
@@ -1,8 +1,16 @@
 module sokoban;
+import sprites;
 
 static unsigned update_data(quack::mapped_buffers all) {
   quack::rect r{};
-  auto count = ui::menu_bg(all, 10, 10, r);
+  auto count = ui::menu_bg(all, 17, 13, r);
+
+  for (auto i = 0; i < sl::max_levels(); i++) {
+    float x = r.x + (i % 10) * 1.5f;
+    float y = r.y + (i / 10) * 1.5f;
+    count += spr::blit::number(all, i + 1, x, y);
+  }
+
   return count;
 }
 
