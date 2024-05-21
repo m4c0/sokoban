03237d2e69b54982dcced95a84e4f8a6dbd6cf39
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-21 12:11:33 +0100

    sselect: menu bg

--- sselect.cpp
+++ sselect.cpp
@@ -12,10 +12,26 @@ static unsigned dim(quack::mapped_buffers &all) {
   return 1;
 }
 
+static unsigned bg(quack::mapped_buffers &all, float w, float h,
+                   quack::colour cl) {
+  auto &[c, m, p, u] = all;
+  float x = sl::level_width - w;
+  float y = sl::level_height - h;
+
+  *p++ = {{x * 0.5f, y * 0.5f}, {w, h}};
+  *u++ = {};
+  *c++ = cl;
+  *m++ = {1, 1, 1, 1};
+  return 1;
+}
+
 static unsigned update_data(quack::mapped_buffers all) {
+  static constexpr const quack::colour diag_bg{0.1, 0.2, 0.3, 0.7};
+
   auto count = sr::update_data(all);
 
   count += dim(all);
+  count += bg(all, 18, 10, diag_bg);
 
   return count;
 }
