58112fd74f65944de7f6a2af37316ddcd88e2eec
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-21 17:27:42 +0100

    sselect: current level highlight

--- sselect.cpp
+++ sselect.cpp
@@ -2,12 +2,19 @@ module sokoban;
 import sprites;
 
 static unsigned update_data(quack::mapped_buffers all) {
+  static constexpr const quack::colour cur_level_bg{0, 0, 0, 1};
+
   quack::rect r{};
   auto count = ui::menu_bg(all, 17, 13, r);
 
   for (auto i = 0; i < sl::max_levels(); i++) {
     float x = r.x + (i % 10) * 1.5f;
     float y = r.y + (i / 10) * 1.5f;
+    if (i == sl::current_level()) {
+      count += ui::bg(all, 1.5, 1, cur_level_bg);
+      all.positions[-1].x = x - 0.75f;
+      all.positions[-1].y = y;
+    }
     count += spr::blit::number(all, i + 1, x, y);
   }
 
