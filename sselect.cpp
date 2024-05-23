b796b66ace22f883596f5f70ff5368619c945e17
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-23 07:51:39 +0100

    sselect: only showing resolved levels

--- sselect.cpp
+++ sselect.cpp
@@ -1,13 +1,15 @@
 module sokoban;
 import sprites;
 
+static unsigned max_level;
+
 static unsigned update_data(quack::mapped_buffers all) {
   static constexpr const quack::colour cur_level_bg{0, 0, 0, 1};
 
   quack::rect r{};
   auto count = ui::menu_bg(all, 17, 13, r);
 
-  for (auto i = 0; i < sl::max_levels(); i++) {
+  for (auto i = 0; i <= max_level; i++) {
     float x = r.x + (i % 10) * 1.5f;
     float y = r.y + (i / 10) * 1.5f;
     if (i == sl::current_level()) {
@@ -22,6 +24,8 @@ static unsigned update_data(quack::mapped_buffers all) {
 }
 
 void open_level_select() {
+  max_level = save::read().max_level;
+
   using namespace casein;
   reset_g(GESTURE);
   reset_k(KEY_DOWN);
