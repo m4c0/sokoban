1ad93821fb39e9dfda02c54a6bb2b0187e7a2dbe
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-19 22:24:18 +0100

    bited/c++: image/dset/etc

--- bited.cpp
+++ bited.cpp
@@ -55,6 +55,8 @@ static constexpr const unsigned buf_size = sizeof(inst) * quad_count;
 static VkDeviceMemory g_mem;
 static unsigned g_inst_count;
 
+static VkImage g_img;
+
 static void refresh_batch() {
   if (!g_mem) return;
 
@@ -83,6 +85,17 @@ static void refresh_batch() {
 }
 
 static void refresh_atlas() {
+  auto img = stbi::load(jojo::slurp("atlas.png"));
+  unsigned w = img.width;
+  unsigned h = img.height;
+  unsigned sz = w * h * 4;
+  auto host = voo::bound_buffer::create_from_host(sz, VK_BUFFER_USAGE_TRANSFER_SRC_BIT);
+  {
+    voo::memiter<unsigned char> c { *host.memory };
+    for (auto i = 0; i < sz; i++) c[i] = (*img.data)[i];
+  }
+
+  voo::copy_buffer_to_image_sync({ w, h }, *host.buffer, g_img);
 }
 
 static void flip_cursor() {
@@ -177,10 +190,13 @@ struct init : vapp {
     main_loop("bited", [&](auto & dq, auto & sw) {
       voo::one_quad oq {};
 
-      vee::descriptor_set_layout dsl = vee::create_descriptor_set_layout({ vee::dsl_fragment_sampler() });
-      vee::pipeline_layout pl = vee::create_pipeline_layout(*dsl, vee::vert_frag_push_constant_range<upc>());
-      vee::render_pass rp = voo::single_att_render_pass(dq);
-      vee::gr_pipeline gp = vee::create_graphics_pipeline({
+      auto dsl = vee::create_descriptor_set_layout({ vee::dsl_fragment_sampler() });
+      auto dpool = vee::create_descriptor_pool(1, { vee::combined_image_sampler(1) });
+      auto dset = vee::allocate_descriptor_set(*dpool, *dsl);
+
+      auto pl = vee::create_pipeline_layout(*dsl, vee::vert_frag_push_constant_range<upc>());
+      auto rp = voo::single_att_render_pass(dq);
+      auto gp = vee::create_graphics_pipeline({
         .pipeline_layout = *pl,
         .render_pass = *rp,
         .shaders {
@@ -204,7 +220,13 @@ struct init : vapp {
       auto buf = voo::bound_buffer::create_from_host(buf_size, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
       g_mem = *buf.memory;
       refresh_batch();
-      //refresh_atlas();
+
+      auto smp = vee::create_sampler(vee::sampler_create_info {}.linear());
+
+      auto img = voo::bound_image::create({ image_w, image_h }, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT);
+      vee::update_descriptor_set(dset, 0, *img.iv, *smp);
+      g_img = *img.img;
+      refresh_atlas();
 
       extent_loop(dq.queue(), sw, [&] {
         sw.queue_one_time_submit([&] {
