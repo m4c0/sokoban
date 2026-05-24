745b0b026cad9972466fcf9294803655aeda5d5b
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-24 21:19:05 +0100

    maped: level wrap

--- c/vlk-maped.h
+++ c/vlk-maped.h
@@ -232,12 +232,12 @@ void vlk_deinit() {
 
 void vlk_load_next_level() {
   int lvl = lvl_current + 1;
-  if (lvl > lvl_max_level) lvl = lvl_max_level;
+  if (lvl > lvl_max_level) lvl = 0;
   vlk_load_map(lvl);
 }
 void vlk_load_prev_level() {
   int lvl = lvl_current - 1;
-  if (lvl < 0) lvl = 0;
+  if (lvl < 0) lvl = lvl_max_level;
   vlk_load_map(lvl);
 }
 
