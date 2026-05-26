a9e13d575ee156922680f2d3f7c3e2238ae44e27
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-26 11:41:45 +0100

    gme: move

--- c/gme.h
+++ c/gme.h
@@ -14,9 +14,62 @@ enum gme_blocks {
 
 extern char * gme_map;
 
+void gme_move(int dx, int dy);
+
 #ifdef GME_IMPL
+#include "lvl.h"
+#include "sfx.h"
 
 char * gme_map;
+
+void gme_move(int dx, int dy) {
+  int px = lvl_px + dx;
+  int py = lvl_py + dy;
+  int p = py * LVL_WIDTH + px;
+
+  int bx = px + dx;
+  int by = py + dy;
+  int b = by * LVL_WIDTH + bx;
+
+  switch (gme_map[p]) {
+    case gme_b_outside:
+    case gme_b_wall:
+      return sfx_fail();
+    case gme_b_empty:
+    case gme_b_target:
+    case gme_b_player:
+    case gme_b_player_target:
+      lvl_px = px; lvl_py = py;
+      break;
+    case gme_b_box:
+    case gme_b_target_box:
+      switch (gme_map[b]) {
+        int open_tgts;
+        case gme_b_empty:
+        case gme_b_target:
+          lvl_px = px; lvl_py = py;
+          gme_map[p] = (gme_map[p] == gme_b_target_box) ? gme_b_target : gme_b_empty;
+          gme_map[b] = (gme_map[b] == gme_b_target) ? gme_b_target_box : gme_b_box;
+
+          open_tgts = 0;
+          for (int i = 0; i < LVL_SZ; i++) {
+            if (gme_map[i] == gme_b_target) open_tgts++;
+          }
+          if (open_tgts == 0) {
+            sfx_eol();
+            lvl_load(lvl_current + 1, gme_map);
+          } else if (gme_map[b] == gme_b_target_box) {
+            sfx_target();
+          } else {
+            sfx_drag();
+          }
+          break;
+        default:
+          return sfx_fail();
+      }
+      break;
+  }
+}
 
 #endif
 #endif
---
+++ c/sfx.h
@@ -0,0 +1,17 @@
+#ifndef SFX_H
+#define SFX_H
+
+static void sfx_eol() {
+  // 50
+}
+static void sfx_target() {
+  // 100
+}
+static void sfx_drag() {
+  // 200
+}
+static void sfx_fail() {
+  // 150
+}
+
+#endif
