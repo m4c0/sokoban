597a5eaa4cd90d32a553b503475ded729ad8d88a
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-26 11:58:46 +0100

    sfx: implementation

--- c/sfx.h
+++ c/sfx.h
@@ -1,6 +1,8 @@
 #ifndef SFX_H
 #define SFX_H
 
+extern int sfx_enabled;
+
 void sfx_filler(float * buf, unsigned sz);
 
 void sfx_drag();
@@ -10,24 +12,40 @@ void sfx_target();
 
 #ifdef SFX_IMPL
 
+int sfx_enabled = 1;
+
+static unsigned sp = 0;
+static unsigned d = 1;
+
 void sfx_filler(float * buf, unsigned sz) {
-  for (int i = 0; i < sz; i++) {
-    buf[i] = 0;
+  int ssp = sp;
+  float mult;
+  if (ssp < 1000) {
+    mult = ssp / 1000.0f;
+  } else if (ssp < 4000) {
+    mult = 1.0;
+  } else if (ssp < 5000) {
+    mult = (5000 - ssp) / 1000.0f;
+  } else {
+    mult = 0;
   }
+  for (unsigned i = 0; i < sz; ++i) {
+    buf[i] = 0.25f * mult * ((ssp / d) % 2) - 0.5f;
+    ssp++;
+  }
+  sp = ssp;
 }
 
-void sfx_drag() {
-  // 200
+static void sfx_play(unsigned div) {
+  if (!sfx_enabled) return;
+
+  d = div;
+  sp = 0;
 }
-void sfx_eol() {
-  // 50
-}
-void sfx_fail() {
-  // 150
-}
-void sfx_target() {
-  // 100
-}
+void sfx_drag()   { sfx_play(200); }
+void sfx_eol()    { sfx_play( 50); }
+void sfx_fail()   { sfx_play(150); }
+void sfx_target() { sfx_play(100); }
 
 #endif
 #endif
