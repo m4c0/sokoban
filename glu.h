60f08bae559e75e3d66f662ecdbe3fd7b88478c1
Eduardo Costa <m4c0@users.noreply.github.com>
2026-08-20 19:09:34 +0100

    glu: upc

--- glu.h
+++ glu.h
@@ -9,9 +9,24 @@
 #define GLU_BUF_SIZE (LVL_SZ * 4)
 
 typedef struct glu_upc_s {
+  float sel_rect_x, sel_rect_y, sel_rect_w, sel_rect_h;
+  float player_pos_x, player_pos_y;
+  float label_pos_x, label_pos_y;
+  float cursor_x, cursor_y;
+  float level;
+  float aspect;
+  float time;
+  float overlay;
+  float back_btn_dim;
+  float menu_btn_dim;
 } glu_upc_t;
-
 glu_upc_t glu_pc;
+
+typedef struct glu_init_s {
+  void * level_data;
+  int sound;
+  int scr_w, scr_h;
+} glu_init_t;
 
 void glu_resize(unsigned w, unsigned h) {
 }
