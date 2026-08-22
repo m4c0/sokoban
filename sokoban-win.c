714bd2919e2a8003ea64dc3c6441ab9af3b8bb28
Eduardo Costa <m4c0@users.noreply.github.com>
2026-08-22 19:21:03 +0100

    win: shoving some d3d code

--- sokoban-win.c
+++ sokoban-win.c
@@ -1,34 +1,370 @@
-#include "gme.h"
-#include "mui.h"
-#include "sfx.h"
-#include "vlk-sokoban.h"
+#include "glu.h"
 
+#define WIN32_LEAN_AND_MEAN
+#include <initguid.h> // Should come first
+
+#include <d3dcompiler.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
 #include <knownfolders.h>
+#include <stdint.h>
+#include <windows.h>
 #include <shlobj.h>
 
-HWND vlk_hwnd;
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "user32.lib")
 
-FILE * vlk_open(const char * name, const char * ext) {
-  char exe[MAX_PATH];
-  GetModuleFileName(NULL, exe, MAX_PATH);
+#define SCR_W 800
+#define SCR_H 600
 
-  char * p = strrchr(exe, '\\');
-  if (p) *p = 0;
+#define BUFFER_COUNT 2
 
-  char buf[MAX_PATH]; snprintf(buf, MAX_PATH, "%s\\%s.%s", exe, name, ext);
-  return fopen(buf, "rb");
+#define DEBUG_INTERFACE
+
+#define COM(obj, method, ...) (obj)->lpVtbl->method(obj, __VA_ARGS__)
+#define COM_OK(obj, method, ...) SUCCEEDED(COM(obj, method, __VA_ARGS__))
+#define COM_CHK(obj, method, ...) if (FAILED(COM(obj, method, __VA_ARGS__))) return 1
+
+static IDXGIFactory4             * d3d_factory;
+static IDXGIAdapter1             * d3d_adapter;
+static ID3D12Device              * d3d_device;
+static ID3D12CommandQueue        * d3d_queue;
+static IDXGISwapChain3           * d3d_swc;
+static ID3D12DescriptorHeap      * d3d_rtv_heap;
+static ID3D12CommandAllocator    * d3d_cmd_alloc;
+static ID3D12GraphicsCommandList * d3d_cmd_list;
+static ID3D12RootSignature       * d3d_root_sign;
+static ID3D12PipelineState       * d3d_pso;
+
+static ID3D12Resource * d3d_rt[BUFFER_COUNT];
+static ID3D12Resource * d3d_buffer;
+
+static ID3D12Fence * d3d_fence;
+static unsigned      d3d_frame_idx;
+static HANDLE        d3d_fence_event;
+static uint64_t      d3d_fence_value;
+
+static void d3d_release(void * obj) {
+  if (obj) COM((IUnknown *)obj, Release);
 }
 
-static char vlk_log_buf[1024];
-void vlk_log(int r, const char * msg) {
-  snprintf(vlk_log_buf, 1024, "Vulkan call failed (code=%d): %s\n", r, msg);
-  MessageBox(NULL, vlk_log_buf, "Vulkan error", 0);
+typedef void (STDMETHODCALLTYPE * d3d_get_cpu_desc_t)(ID3D12DescriptorHeap *, D3D12_CPU_DESCRIPTOR_HANDLE *);
+static D3D12_CPU_DESCRIPTOR_HANDLE d3d_get_cpu_desc(ID3D12DescriptorHeap * heap) {
+  D3D12_CPU_DESCRIPTOR_HANDLE h;
+  ((d3d_get_cpu_desc_t)heap->lpVtbl->GetCPUDescriptorHandleForHeapStart)(heap, &h);
+  return h;
+}
 
-  // This is the only way to properly programatically exit an app from any
-  // thread. Other attempts froze the app or kept it as a "background app".
-  // i.e. We use WM_CLOSE instead of WM_QUIT (or PostQuitMessage etc) and we
-  // need to use SendNotifyMessage instead of SendMessage.
-  if (vlk_hwnd) SendNotifyMessage(vlk_hwnd, WM_CLOSE, 0, 0);
+static int d3d_debug() {
+#ifdef DEBUG_INTERFACE
+  ID3D12Debug * debug;
+  if (SUCCEEDED(D3D12GetDebugInterface(&IID_ID3D12Debug, (void **)&debug))) {
+    COM(debug, EnableDebugLayer);
+    return DXGI_CREATE_FACTORY_DEBUG;
+  }
+#endif
+  return 0;
+}
+
+static inline int d3d_enum_adapter_by_gpu(IDXGIFactory6 * f6, unsigned i) {
+  return COM_OK(f6, EnumAdapterByGpuPreference, i, DXGI_GPU_PREFERENCE_UNSPECIFIED, &IID_IDXGIAdapter1, (void **)&d3d_adapter);
+}
+static inline int d3d_enum_adapter(unsigned i) {
+  return COM_OK(d3d_factory, EnumAdapters1, i, &d3d_adapter);
+}
+static inline int d3d_adapter_is_software(void) {
+  DXGI_ADAPTER_DESC1 desc;
+  COM(d3d_adapter, GetDesc1, &desc);
+  return desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE;
+}
+static inline int d3d_create_device(void) {
+  return FAILED(D3D12CreateDevice((IUnknown *)d3d_adapter, D3D_FEATURE_LEVEL_11_0, &IID_ID3D12Device, (void **)&d3d_device));
+}
+static int d3d_init_adapter(void) {
+  IDXGIFactory6 * factory6;
+  if (COM_OK(d3d_factory, QueryInterface, &IID_IDXGIFactory6, (void **)&factory6)) {
+    for (unsigned i = 0; d3d_enum_adapter_by_gpu(factory6, i); i++) {
+      if (d3d_adapter_is_software()) continue;
+      if (0 == d3d_create_device()) return 0;
+    }
+  }
+
+  for (unsigned i = 0; d3d_enum_adapter(i); i++) {
+    if (d3d_adapter_is_software()) continue;
+    if (0 == d3d_create_device()) return 0;
+  }
+
+  COM_CHK(d3d_factory, EnumWarpAdapter, &IID_IDXGIAdapter1, (void **)&d3d_adapter);
+  return d3d_create_device();
+}
+
+static int d3d_init_queue(void) {
+  D3D12_COMMAND_QUEUE_DESC desc = {0};
+  COM_CHK(d3d_device, CreateCommandQueue, &desc, &IID_ID3D12CommandQueue, (void **)&d3d_queue);
+  return 0;
+}
+
+static int d3d_init_swapchain(HWND hwnd) {
+  IDXGISwapChain1 * swc;
+  DXGI_SWAP_CHAIN_DESC1 desc = {
+    .Width       = SCR_W,
+    .Height      = SCR_H,
+    .Format      = DXGI_FORMAT_R8G8B8A8_UNORM,
+    .BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
+    .BufferCount = BUFFER_COUNT,
+    .SwapEffect  = DXGI_SWAP_EFFECT_FLIP_DISCARD,
+
+    .SampleDesc = (DXGI_SAMPLE_DESC) {
+      .Count = 1,
+    },
+  };
+  COM_CHK(d3d_factory, CreateSwapChainForHwnd, (IUnknown *)d3d_queue, hwnd, &desc, NULL, NULL, &swc);
+  COM_CHK(swc, QueryInterface, &IID_IDXGISwapChain3, (void **)&d3d_swc);
+  return 0;
+}
+
+static int d3d_init_rtv_heap(void) {
+  D3D12_DESCRIPTOR_HEAP_DESC desc = {
+    .Type           = D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+    .NumDescriptors = BUFFER_COUNT,
+  };
+  COM_CHK(d3d_device, CreateDescriptorHeap, &desc, &IID_ID3D12DescriptorHeap, (void **)&d3d_rtv_heap);
+  return 0;
+}
+
+static D3D12_CPU_DESCRIPTOR_HANDLE d3d_get_rtv_cpu_desc(int i) {
+  D3D12_CPU_DESCRIPTOR_HANDLE h = d3d_get_cpu_desc(d3d_rtv_heap);
+  h.ptr += i * COM(d3d_device, GetDescriptorHandleIncrementSize, D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+  return h;
+}
+
+static int d3d_init_rtv(void) {
+  for (int i = 0; i < BUFFER_COUNT; i++) {
+    COM_CHK(d3d_swc, GetBuffer, i, &IID_ID3D12Resource, (void **)&d3d_rt[i]);
+    COM(d3d_device, CreateRenderTargetView, d3d_rt[i], NULL, d3d_get_rtv_cpu_desc(i));
+  }
+  return 0;
+}
+
+static int d3d_init_cmdlist() {
+  COM_CHK(d3d_device, CreateCommandList, 0, D3D12_COMMAND_LIST_TYPE_DIRECT, d3d_cmd_alloc, d3d_pso, &IID_ID3D12GraphicsCommandList, (void **)&d3d_cmd_list);
+  COM_CHK(d3d_cmd_list, Close);
+  return 0;
+}
+
+static void d3d_report_err(ID3DBlob * err) {
+  if (!err) return;
+  const char * txt = COM(err, GetBufferPointer);
+  MessageBox(NULL, txt, "Direct3D error", MB_ICONERROR);
+}
+
+static int d3d_init_root_signature() {
+  ID3DBlob * blob;
+  ID3DBlob * err;
+  D3D12_ROOT_SIGNATURE_DESC desc = {
+    .NumParameters      = 2,
+    .pParameters        = (D3D12_ROOT_PARAMETER[]) {{
+      .ParameterType    = D3D12_ROOT_PARAMETER_TYPE_SRV,
+    }, {
+      .ParameterType    = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS,
+      .Constants        = (D3D12_ROOT_CONSTANTS) {
+        .Num32BitValues = sizeof(glu_upc_t) / 4,
+      },
+    }},
+  };
+  if (FAILED(D3D12SerializeRootSignature(&desc, D3D_ROOT_SIGNATURE_VERSION_1_0, &blob, &err))) return (d3d_report_err(err), 1);
+  if (err) return (d3d_report_err(err), 1);
+
+  const void * data = COM(blob, GetBufferPointer);
+  size_t        len = COM(blob, GetBufferSize);
+  COM_CHK(d3d_device, CreateRootSignature, 0, data, len, &IID_ID3D12RootSignature, (void **)&d3d_root_sign);
+
+  d3d_release(blob);
+  d3d_release(err);
+  return 0;
+}
+
+static ID3DBlob * d3d_compile(const char * tgt, const char * name) {
+  HRSRC r = FindResource(NULL, name, "hlsl");
+  HGLOBAL g = LoadResource(NULL, r);
+  void * ptr = LockResource(g);
+  unsigned sz = SizeofResource(NULL, r);
+
+  ID3DBlob * blob;
+  ID3DBlob * err;
+  if (FAILED(D3DCompile(ptr, sz, NULL, NULL, NULL, "main", tgt, 0, 0, &blob, &err))) return (d3d_report_err(err), NULL);
+  // If you want warnings as well:
+  // if (err) return (d3d_report_err(err), NULL);
+  return blob;
+}
+static D3D12_SHADER_BYTECODE d3d_blob2shader(ID3DBlob * blob) {
+  return (D3D12_SHADER_BYTECODE){ COM(blob, GetBufferPointer), COM(blob, GetBufferSize) };
+}
+static int d3d_init_pso() {
+  ID3DBlob * vs = d3d_compile("vs_5_0", "shader.vert");
+  ID3DBlob * ps = d3d_compile("ps_5_0", "shader.frag");
+  if (!vs || !ps) return 1;
+
+  D3D12_GRAPHICS_PIPELINE_STATE_DESC desc = {
+    .pRootSignature        = d3d_root_sign,
+    .VS                    = d3d_blob2shader(vs),
+    .PS                    = d3d_blob2shader(ps),
+    .PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE,
+    .SampleMask            = UINT_MAX,
+    .NumRenderTargets      = 1,
+
+    .RasterizerState = (D3D12_RASTERIZER_DESC) {
+      .FillMode = D3D12_FILL_MODE_SOLID,
+      .CullMode = D3D12_CULL_MODE_NONE,
+    },
+    .SampleDesc = (DXGI_SAMPLE_DESC) {
+      .Count = 1,
+    },
+  };
+  desc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+  desc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+  COM_CHK(d3d_device, CreateGraphicsPipelineState, &desc, &IID_ID3D12PipelineState, (void **)&d3d_pso);
+
+  d3d_release(vs);
+  d3d_release(ps);
+  return 0;
+}
+
+static int d3d_init_buffer(void) {
+  int size = GLU_BUF_SIZE;
+
+  D3D12_HEAP_PROPERTIES heap = {
+    .Type = D3D12_HEAP_TYPE_UPLOAD,
+  };
+  D3D12_RESOURCE_DESC res = {
+    .Dimension        = D3D12_RESOURCE_DIMENSION_BUFFER,
+    .Width            = size,
+    .Height           = 1,
+    .DepthOrArraySize = 1,
+    .MipLevels        = 1,
+    .Layout           = D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+    .SampleDesc       = (DXGI_SAMPLE_DESC) {
+      .Count          = 1,
+    },
+  };
+  COM_CHK(d3d_device, CreateCommittedResource,
+      &heap, D3D12_HEAP_FLAG_NONE, &res, D3D12_RESOURCE_STATE_GENERIC_READ, NULL, 
+      &IID_ID3D12Resource, (void **)&d3d_buffer);
+
+  return 0;
+}
+
+int d3d_init(HWND hwnd) {
+  if (FAILED(CreateDXGIFactory2(d3d_debug(), &IID_IDXGIFactory4, (void **)&d3d_factory))) return 1;
+
+  if (d3d_init_adapter())       return 1;
+  if (d3d_init_queue())         return 1;
+  if (d3d_init_swapchain(hwnd)) return 1;
+
+  COM_CHK(d3d_factory, MakeWindowAssociation, hwnd, DXGI_MWA_NO_ALT_ENTER);
+
+  if (d3d_init_rtv_heap()) return 1;
+  if (d3d_init_rtv())      return 1;
+
+  COM_CHK(d3d_device, CreateCommandAllocator, D3D12_COMMAND_LIST_TYPE_DIRECT, &IID_ID3D12CommandAllocator, (void **)&d3d_cmd_alloc);
+
+  if (d3d_init_root_signature()) return 1;
+  if (d3d_init_pso())            return 1;
+  if (d3d_init_cmdlist())        return 1;
+
+  if (d3d_init_buffer()) return 1;
+
+  COM_CHK(d3d_device, CreateFence, 0, D3D12_FENCE_FLAG_NONE, &IID_ID3D12Fence, (void **)&d3d_fence);
+  d3d_frame_idx   = COM(d3d_swc, GetCurrentBackBufferIndex);
+  d3d_fence_value = 1;
+  d3d_fence_event = CreateEvent(NULL, FALSE, FALSE, NULL);
+  if (!d3d_fence_event) return 1;
+
+  return 0;
+}
+
+static int d3d_wait() {
+  uint64_t v = d3d_fence_value;
+  COM_CHK(d3d_queue, Signal, d3d_fence, v);
+  d3d_fence_value++;
+
+  if (COM(d3d_fence, GetCompletedValue) < v) {
+    COM(d3d_fence, SetEventOnCompletion, v, d3d_fence_event);
+    WaitForSingleObject(d3d_fence_event, INFINITE);
+  }
+
+  d3d_frame_idx = COM(d3d_swc, GetCurrentBackBufferIndex);
+  return 0;
+}
+
+void d3d_deinit(void) {
+  d3d_wait();
+
+  for (int i = 0; i < BUFFER_COUNT; i++) d3d_release(d3d_rt[i]);
+
+  d3d_release(d3d_fence);
+
+  d3d_release(d3d_buffer);
+  d3d_release(d3d_cmd_list);
+  d3d_release(d3d_pso);
+  d3d_release(d3d_root_sign);
+  d3d_release(d3d_cmd_alloc);
+  d3d_release(d3d_rtv_heap);
+  d3d_release(d3d_swc);
+  d3d_release(d3d_queue);
+  d3d_release(d3d_device);
+  d3d_release(d3d_adapter);
+  d3d_release(d3d_factory);
+
+  CloseHandle(d3d_fence_event);
+}
+
+static void d3d_cmd_transition_barrier(D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after) {
+  D3D12_RESOURCE_BARRIER b = {
+    .Transition    = {
+      .pResource   = d3d_rt[d3d_frame_idx],
+      .StateBefore = before,
+      .StateAfter  = after,
+    }
+  };
+  COM(d3d_cmd_list, ResourceBarrier, 1, &b);
+}
+int d3d_frame(void) {
+  COM_CHK(d3d_cmd_alloc, Reset);
+  COM_CHK(d3d_cmd_list, Reset, d3d_cmd_alloc, d3d_pso);
+
+  COM(d3d_cmd_list, SetGraphicsRootSignature, d3d_root_sign);
+  COM(d3d_cmd_list, SetGraphicsRootShaderResourceView, 0, COM(d3d_buffer, GetGPUVirtualAddress));
+  COM(d3d_cmd_list, SetGraphicsRoot32BitConstants, 1, sizeof(glu_upc_t) / 4, &glu_pc, 0);
+
+  D3D12_VIEWPORT vp = { 0, 0, SCR_W, SCR_H };
+  COM(d3d_cmd_list, RSSetViewports, 1, &vp);
+  D3D12_RECT     sc = { 0, 0, SCR_W, SCR_H };
+  COM(d3d_cmd_list, RSSetScissorRects, 1, &sc);
+
+  d3d_cmd_transition_barrier(D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+
+  D3D12_CPU_DESCRIPTOR_HANDLE rtv = d3d_get_rtv_cpu_desc(d3d_frame_idx);
+  COM(d3d_cmd_list, OMSetRenderTargets, 1, &rtv, FALSE, NULL);
+
+  float colour[] = { 0.1, 0.2, 0.3, 1.0 };
+  COM(d3d_cmd_list, ClearRenderTargetView, rtv, colour, 0, NULL);
+  COM(d3d_cmd_list, IASetPrimitiveTopology, D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+  COM(d3d_cmd_list, DrawInstanced, 3, 1, 0, 0);
+
+  d3d_cmd_transition_barrier(D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+
+  COM_CHK(d3d_cmd_list, Close);
+
+  ID3D12CommandList * cmd_list = (ID3D12CommandList *)d3d_cmd_list;
+  COM(d3d_queue, ExecuteCommandLists, 1, &cmd_list);
+
+  COM_CHK(d3d_swc, Present, 1, 0);
+  d3d_wait();
+
+  return 0;
 }
 
 void sav_get_path(char * buf, unsigned sz) {
@@ -65,23 +401,18 @@ void sfx_save_prefs() {
 
 static LRESULT window_proc(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param) {
   switch (msg) {
-    case WM_CLOSE:
-      // Required to enable another thread sending "plz exit" messages
-      DestroyWindow(hwnd);
-      return 0;
     case WM_DESTROY:
-      vlk_deinit();
       PostQuitMessage(0);
       return 0;
 
     case WM_MOUSEMOVE:
-      mu_input_mousemove(&mui_ctx, GET_X_LPARAM(l_param), GET_Y_LPARAM(l_param));
+      glu_mouse_move(GET_X_LPARAM(l_param), GET_Y_LPARAM(l_param));
       return 0;
     case WM_LBUTTONDOWN:
-      mu_input_mousedown(&mui_ctx, GET_X_LPARAM(l_param), GET_Y_LPARAM(l_param), 1);
+      glu_mouse_down(GET_X_LPARAM(l_param), GET_Y_LPARAM(l_param));
       return 0;
     case WM_LBUTTONUP:
-      mu_input_mouseup(&mui_ctx, GET_X_LPARAM(l_param), GET_Y_LPARAM(l_param), 1);
+      glu_mouse_up(GET_X_LPARAM(l_param), GET_Y_LPARAM(l_param));
       return 0;
 
     case WM_KEYDOWN:
@@ -96,18 +427,16 @@ static LRESULT window_proc(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param) 
 
       return 0;
 
-    case WM_ERASEBKGND:
-      // i.e. "never erase background". Solves 99.999% of flicker issues
-      return 1;
     case WM_PAINT:
-      if (vlk_hwnd) vlk_frame();
+      glu_frame();
+      if (d3d_frame()) PostQuitMessage(1);
       return 0;
   }
   return DefWindowProc(hwnd, msg, w_param, l_param);
 }
 
 int WinMain(HINSTANCE h_instance, HINSTANCE h_prev, LPSTR cmd_line, int cmd_show) {
-  HICON h_icon = LoadIcon(h_instance, "IDI_ICON");
+  HICON h_icon = LoadIcon(h_instance, "IDI_APPICON");
 
   WNDCLASSEX wcex  = {
     .cbSize        = sizeof(WNDCLASSEX),
@@ -117,7 +446,7 @@ int WinMain(HINSTANCE h_instance, HINSTANCE h_prev, LPSTR cmd_line, int cmd_show
     .hIcon         = h_icon,
     .hCursor       = LoadCursor(NULL, IDC_ARROW),
     .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
-    .lpszClassName = "m4c0-sokoban-window",
+    .lpszClassName = "m4c0-window",
     .hIconSm       = h_icon,
   };
   if (!RegisterClassEx(&wcex)) {
@@ -127,16 +456,24 @@ int WinMain(HINSTANCE h_instance, HINSTANCE h_prev, LPSTR cmd_line, int cmd_show
 
   DWORD style = WS_OVERLAPPEDWINDOW ^ WS_SIZEBOX ^ WS_MAXIMIZEBOX;
 
+  char title[256];
+  LoadString(h_instance, 101, title, sizeof(title));
+
   HWND hwnd = CreateWindow(
-      "m4c0-sokoban-window",
-      "Casually Casual Warehouse Game",
+      "m4c0-window", title,
       style, CW_USEDEFAULT, CW_USEDEFAULT,
-      800, 600, 
+      SCR_W, SCR_H, 
       NULL, NULL, h_instance, NULL);
   if (!hwnd) {
     MessageBox(NULL, "Failed to create window", "Unhandled error", 0);
     return 1;
   }
+
+  if (d3d_init(hwnd)) return 1;
+
+  RECT rect;
+  GetClientRect(hwnd, &rect);
+  glu_init(rect.right - rect.left, rect.bottom - rect.top);
 
   ShowWindow(hwnd, cmd_show);
   UpdateWindow(hwnd);
@@ -146,13 +483,13 @@ int WinMain(HINSTANCE h_instance, HINSTANCE h_prev, LPSTR cmd_line, int cmd_show
   RegQueryValueExA(sfx_key(), "sound", NULL, NULL, (void *)&val, &size);
   sfx_init(val ? 1 : 0);
 
-  vlk_hwnd = hwnd;
-  vlk_init();
-
   MSG msg;
   while (GetMessage(&msg, 0, 0, 0)) {
     TranslateMessage(&msg);
     DispatchMessage(&msg);
   }
+
+  glu_deinit();
+  d3d_deinit();
   return msg.wParam;
 }
